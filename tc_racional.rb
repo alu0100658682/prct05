fb0145a7b2370aab3993abc59fb226c1bddde4b6
Jacobo Saavedra <alu0100658682@ull.edu.es>
2013-10-18 13:33:37 +0100

    prct05

--- tc_racional.rb
+++ tc_racional.rb
@@ -5,7 +5,14 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
         
-      
+        def test_1
+        
+                assert_equal "3/4", (Fraccion.new(2,4) + Fraccion.new(1,4)).to_s        
+                assert_equal "1/4", (Fraccion.new(2,4) - Fraccion.new(1,4)).to_s
+                assert_equal "1/6", (Fraccion.new(2,6) * Fraccion.new(4,8)).to_s
+                assert_equal "8/3", (Fraccion.new(4,6) / Fraccion.new(2,8)).to_s
+                
+        end
 
 end
 
